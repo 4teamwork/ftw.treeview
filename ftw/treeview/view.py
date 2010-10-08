302b76cf7a8ecc6b9191cc103a39919b62981fe0
Philippe Gross <p.gross@4teamwork.ch>
2010-10-08 11:39:23 +0000

    fixed the tree portlet so that it work also with two repositorysystems

--- ftw/treeview/view.py
+++ ftw/treeview/view.py
@@ -1,50 +1,51 @@
-from Products.CMFPlone.browser.navtree import NavtreeQueryBuilder
 from Products.CMFPlone.browser.navigation import CatalogNavigationTree
+from Products.CMFPlone.utils import getToolByName
 from plone.app.layout.navigation.interfaces import INavtreeStrategy
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from zope.component import getMultiAdapter
 from Acquisition import aq_inner
-
 from navtree import  buildFolderTree
-
 
 
 class TreeView(CatalogNavigationTree):
 
     recurse = ViewPageTemplateFile('recurse.pt')
-    
-    def render(self):
+
+    def render(self, root_path=None):
         """return a html tree for treeview"""
-        
-        current = context = aq_inner(self.context)
+        if root_path:
+            portal_url = getToolByName(self.context, 'portal_url')
+            current = portal_url.getPortalObject().restrictedTraverse(
+                root_path.encode('utf-8'))
+
+            if ('/'.join(current.getPhysicalPath())+'/').startswith(
+                    '/'.join(self.context.getPhysicalPath())+'/'):
+                context = aq_inner(self.context)
+                return self.get_tree(context, current)
+            else:
+                return self.get_tree(current, current)
+        else:
+            current = context = aq_inner(self.context)
+            return self.get_tree(context, current)
+
+    def get_tree(self, context, current):
+        self.context = context
         # Don't travsere to top-level application obj if TreePortlet
         # was added to the Plone Site Root
         if current.Type() == 'Plone Site':
             return current.Title()
-        else:  
+        else:
             while current.Type() != 'RepositoryRoot':
                 current = current.aq_parent
-        return current.Title()
 
-
-        queryBuilder = NavtreeQueryBuilder(context)
-
-        #XXX use querybuilder... 
-        #query = queryBuilder()
-        query = {'path': dict(query='/'.join(current.getPhysicalPath()), depth=-1), 
-                 'Type': 'RepositoryFolder'}
-        strategy = getMultiAdapter((context, self), INavtreeStrategy)
-        data = buildFolderTree(context, obj=context, query=query, strategy=strategy)
+        query = {
+            'path': dict(query='/'.join(current.getPhysicalPath()), depth=-1),
+            'Type': 'RepositoryFolder'}
+        strategy = getMultiAdapter((context.aq_inner, self), INavtreeStrategy)
+        #import pdb; pdb.set_trace( )
+        data = buildFolderTree(context.aq_inner,
+            obj=context.aq_inner, query=query, strategy=strategy)
         children = data.get('children')[0].get('children')
-        html=self.recurse(children=children,level=1, bottomLevel=999)
+
+        html=self.recurse(children=children, level=1, bottomLevel=999)
         return html
-        # queryBuilder = NavtreeQueryBuilder
-        # strategy = getMultiAdapter((aq_inner(self.context), self),
-        #                            INavtreeStrategy)
-        # 
-        # query=queryBuilder()
-        # 
-        # data=buildFolderTree(root, query=query, strategy=strategy)
-        # html=self.recurse(children=data.get('children', []), level=1, bottomLevel=0)
-        # 
-        # return html.encode('utf8')
